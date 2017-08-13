11721feb73bff138730ac5b6c253b833ff6757ac
Vedran Abramovic <vedran.abramovic@ag04.com>
2017-08-13 20:03:27 +0200

    Created analysis service and repository

---
+++ src/main/java/com/vabram/hearthum/service/impl/AnalysisServiceImpl.java
@@ -0,0 +1,26 @@
+package com.vabram.hearthum.service.impl;
+
+import com.vabram.hearthum.model.Analysis;
+import com.vabram.hearthum.model.Analyzer;
+import com.vabram.hearthum.repository.AnalysisRepository;
+import com.vabram.hearthum.repository.AnalyzerRepository;
+import com.vabram.hearthum.service.AnalysisService;
+import com.vabram.hearthum.service.AnalyzerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * Created by brama051 on 1.3.2017..
+ */
+@Service
+public class AnalysisServiceImpl implements AnalysisService {
+
+    @Autowired
+    private AnalysisRepository analysiseRpository;
+
+    @Override
+    public Analysis findOne(Long id) {
+        return analysiseRpository.findOne(id);
+    }
+}
